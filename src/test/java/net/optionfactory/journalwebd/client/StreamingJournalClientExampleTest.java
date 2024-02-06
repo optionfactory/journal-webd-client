e93c44bc14ddcb8326273b054634dbff31163016
Roberto Ferranti <roberto.ferranti@gmail.com>
2024-02-06 17:27:26 +0100

    :new: version 1.0

--- src/test/java/net/optionfactory/journalwebd/client/StreamingJournalClientExampleTest.java
+++ src/test/java/net/optionfactory/journalwebd/client/StreamingJournalClientExampleTest.java
@@ -4,11 +4,13 @@ package net.optionfactory.journalwebd.client;
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class StreamingJournalClientExampleTest {
 
     @Test
+    @Ignore
     public void canCallJournalWebd() throws InterruptedException, ExecutionException {
         final var token = "TEST_TOKEN";
         final var entries = new StreamingJournalClient(URI.create("ws://localhost:8000/ws/stream"), token)
