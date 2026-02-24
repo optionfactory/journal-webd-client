1f95b1e5951b6dad369d41dbaec6aad0ef6103a8
Roberto Ferranti <roberto.ferranti@gmail.com>
2026-02-24 13:32:42 +0100

    :ref: bumped dependencies

--- src/test/java/net/optionfactory/journalwebd/client/StreamingJournalClientExampleTest.java
+++ src/test/java/net/optionfactory/journalwebd/client/StreamingJournalClientExampleTest.java
@@ -4,13 +4,13 @@ import java.net.URI;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import net.optionfactory.journalwebd.client.StreamingJournalClient.Configuration;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class StreamingJournalClientExampleTest {
 
     @Test
-    @Ignore
+    @Disabled
     public void canCallJournalWebd() throws InterruptedException, ExecutionException {
         final var uri = URI.create("ws://localhost:8000/ws/stream");
         final var token = "TEST_TOKEN";
